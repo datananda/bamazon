1ea414a64e2d84ed4a02eb325221e313d51bdadd
Madeleine Filloux <mfilloux@gmail.com>
2018-05-05 20:26:38 -0500

    refactor and finalize manager options

--- bamazonManager.js
+++ bamazonManager.js
@@ -16,112 +16,102 @@ const productTable = new Table({
     colWidths: [5, 40, 10, 10],
 });
 
-function addProduct(product, price, quantity) {
-    connection.query("INSERT INTO products (product_name,", [numUnits, id], (err) => {
+function validateNumUnits(units) {
+    if (/^[1-9]\d*$/.test(units)) {
+        return true;
+    }
+    return "Please enter a number greater than zero.";
+}
+
+function validateID(id) {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT product_name FROM products WHERE item_id = ?", [id], (err, res) => {
+            if (err) throw err;
+            if (res.length > 0) {
+                resolve(true);
+            } else {
+                reject("Please enter a valid product id.");
+            }
+        });
+    });
+}
+
+function validatePrice(price) {
+    const priceFloat = parseFloat(price);
+    if (!Number.isNaN(priceFloat) && priceFloat > 0) {
+        return true;
+    }
+    return "Please enter a number greater than zero. Do not enter $ or any other extra symbols.";
+}
+
+function addProduct(product, dept, price, quantity) {
+    const records = [
+        [product, dept, price, quantity],
+    ];
+    connection.query("INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ?", [records], (err) => {
         if (err) throw err;
+        console.log(`\nYou added ${product} to the ${dept} department's inventory with a stock quantity of ${quantity} and a price of $${price}.`);
         connection.end();
     });
 }
 
-function newQuantityPrompt(product, price) {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: `What is the stock quantity of ${product}?`,
-            name: "productStock",
-        },
-    ]).then((response) => {
-        if (/^[1-9]\d*$/.test(response.productStock)) {
-            console.log("valid number");
-        } else {
-            console.log("\nPlease enter a number greater than zero.");
-            newQuantityPrompt(product);
-        }
-    });
-}
-
-function newPricePrompt(product) {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: `What is the price of ${product}?`,
-            name: "productPrice",
-        },
-    ]).then((response) => {
-        const priceFloat = parseFloat(response.productPrice);
-        if (!Number.isNaN(priceFloat) && priceFloat > 0) {
-            newQuantityPrompt(product, priceFloat);
-        } else {
-            console.log("\nPlease enter a number greater than zero. Do not enter $ or any other extra symbols.");
-            newPricePrompt(product);
-        }
-    });
-}
-
-function newProductPrompt() {
+function newPrompt() {
     inquirer.prompt([
         {
             type: "input",
             message: "What is the name of the product you would like to add?",
             name: "productName",
+            validate: input => input !== "",
+        },
+        {
+            type: "input",
+            message: "What department does the new product belong to?",
+            name: "deptName",
+            validate: input => input !== "",
+        },
+        {
+            type: "input",
+            message: "What is the price per unit of the new product?",
+            name: "price",
+            validate: validatePrice,
+        },
+        {
+            type: "input",
+            message: "What is the stock quantity of the new product?",
+            name: "stock",
+            validate: validateNumUnits,
+            filter: val => parseInt(val, 10),
         },
     ]).then((response) => {
-        if (response.productName) {
-            newPricePrompt(response.productName);
-        } else {
-            console.log("\nYou must enter a product name.");
-            newProductPrompt();
-        }
+        addProduct(response.productName, response.deptName, response.price, response.stock);
     });
 }
 
 function addInventory(id, numUnits) {
     connection.query("UPDATE products SET stock_quantity = stock_quantity + ? WHERE item_id = ?", [numUnits, id], (err) => {
         if (err) throw err;
+        console.log(`\nYou added ${numUnits} ${numUnits === 1 ? "unit" : "units"} of product id ${id}.`);
         connection.end();
     });
 }
 
-function promptForNumUnits(id, product) {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: `How many units of ${product} would you like to add to stock?`,
-            name: "numUnitsChoice",
-        },
-    ]).then((response) => {
-        if (/^[1-9]\d*$/.test(response.numUnitsChoice)) {
-            const numUnits = parseInt(response.numUnitsChoice, 10);
-            console.log(`\nYou added ${numUnits} ${numUnits === 1 ? "unit" : "units"} of ${product}.`);
-            addInventory(id, numUnits);
-        } else {
-            console.log("\nPlease enter a number greater than zero.");
-            promptForNumUnits(id, product);
-        }
-    });
-}
-
-function checkForProduct(id) {
-    connection.query("SELECT product_name FROM products WHERE item_id = ?", [id], (err, res) => {
-        if (err) throw err;
-        if (res.length > 0) {
-            promptForNumUnits(id, res[0].product_name);
-        } else {
-            console.log("\nThat is not a valid product ID. Please try again.");
-            promptForID();
-        }
-    });
-}
-
-function promptForID() {
+function addPrompt() {
     inquirer.prompt([
         {
             type: "input",
             message: "What is the ID of the product you would like to add stock to?",
-            name: "productChoice",
+            name: "productID",
+            validate: validateID,
+        },
+        {
+            type: "input",
+            message: "How many units are you adding to stock?",
+            name: "addUnits",
+            validate: validateNumUnits,
+            filter: val => parseInt(val, 10),
         },
     ]).then((response) => {
-        checkForProduct(response.productChoice);
+        addInventory(response.productID, response.addUnits);
     });
 }
 
@@ -161,7 +151,6 @@ function promptForChoice() {
             ],
         },
     ]).then((response) => {
-        console.log(response);
         switch (response.actionChoice) {
         case "View products for sale":
             viewProducts();
@@ -170,10 +159,10 @@ function promptForChoice() {
             viewLowInventory();
             break;
         case "Add to inventory":
-            promptForID();
+            addPrompt();
             break;
         case "Add new product":
-            newProductPrompt();
+            newPrompt();
             break;
         default:
             console.log("Sorry. That is not an option. Try again.");
